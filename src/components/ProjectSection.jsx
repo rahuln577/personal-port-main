8f0e8c6ec0a106317e51ad03f1f6945d5eb7d1df
rahuln577 <rahulrahuln2001@gmail.com>
2023-10-13 22:34:04 +0530

    Projects update

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,6 +1,8 @@
 import react ,{useState,useEffect,useRef} from "react"
 import reactdom from "react-dom"
 import foodblog from "./../images/foodblog.png"
+import moviedb from "./../images/moviedb.png"
+import tech from "./../images/tech.png"
 import ProjectCard from "./project-card"
 function ProjectSection()
 
@@ -16,9 +18,9 @@ function ProjectSection()
     return <div ref ={myref}className={`project ${isset?`scanim`:` `}`} id="project">
         <h1>Projects</h1>
         <div className="plist">
-        <ProjectCard image={foodblog} title="Food Blog website" description="This is a platform where users can share their experiences with different food." github="www.google.com" live="www.google.com"/>
-        <ProjectCard image={foodblog} title="Food Blog website" description="This is a platform where users can share their experiences with different food." github="www.google.com" live="www.google.com"/>
-        <ProjectCard image={foodblog} title="Food Blog website" description="This is a platform where users can share their experiences with different food." github="www.google.com" live="www.google.com"/>
+        <ProjectCard image={foodblog} title="Food Blog website" description="This is a platform where users can share their experiences with different food." github="https://github.com/rahuln577/Personal-Blog-Food-Blog" live="nrahul.netlify.app"/>
+        <ProjectCard image={moviedb} title="MovieDB website" description="This is a website which has description of all the latest movies, basically a database for movies." github="https://github.com/rahuln577/moviedbfinal" live="moviedbbyrahul.netlify.app"/>
+        <ProjectCard image={tech} title="TechTuneMotors website" description="This is a website for mechanic shop called TechTune Motors which allows the user to book different services offered." github="https://github.com/rahuln577/TechTuneMotors" live="http://techtunemotors.netlify.app"/>
     </div>
     </div>
 }
